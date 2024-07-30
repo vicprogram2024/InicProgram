c3f1167b8616cd10f8c68784ca4864e8ddccad58
vicprogram2024 <168384662+vicprogram2024@users.noreply.github.com>
2024-07-30 16:30:59 -0400

    mi primer archivo creado

---
+++ primer_ejercicio.py
@@ -0,0 +1,30 @@
+# Comentario en una linea. https://www.python.org
+ 
+# Para realizar un comentario de una solo linea se una el numeral
+
+"""Para el comentario en varias lineas 
+se utiliza tres comillas seguidas"""
+
+# Para crear una variable basta con agregar el nombre seguido del igual
+
+my_variable= "Mi variable"
+my_variable= "Nuevo valor de mi variable"
+
+""" En python no existen las constantes. Para representarla se utiliza en mayuscula 
+el nombre de la variable """
+
+MY_CONSTANT= "Mi constante, por conversion. Esta constante no debe ser cambiada"
+
+# Variables de tipos de datos primitivos 
+
+my_int = 1
+my_float = 1.5
+my_string = "Mi cadena de texto"
+my_bool = True
+my_bool = False
+
+# Imprimir terminal de texto: "Hola Python"
+
+print ("Hola Python de maravilla ")
+
+print(type(my_int))
